f9abaa060a40a12b01ff0975ab268c579857d386
furkan-cloud <firstinfirstout2020@gmail.com>
2020-11-05 23:05:02 +0300

    searchbar component and marker added

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import MapView from 'react-native-maps';
+import Axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 import {City, RestaurantDetail, SearchBar} from './components';
 
+let originalList = [];
+
 const Main = (props) => {
+  const [cityList, setCityList] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+
+  const fetchCities = async () => {
+    const {data} = await Axios.get(
+      'https://opentable.herokuapp.com/api/cities',
+    );
+    setCityList(data.cities);
+    originalList = [...data.cities];
+  };
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
+  const onCitySearch = (text) => {
+    const filteredList = originalList.filter((item) => {
+      const userText = text.toUpperCase();
+      const cityName = item.toUpperCase();
+
+      return cityName.indexOf(userText) > -1;
+    });
+
+    setCityList(filteredList);
+  };
+
+  const onCitySelect = async (city) => {
+    const {
+      data: {restaurants},
+    } = await Axios.get(
+      'https://opentable.herokuapp.com/api/restaurants?city=' + city,
+    );
+    setRestaurants(restaurants)
+    console.log(restaurants);
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1}}>
@@ -14,13 +53,31 @@ const Main = (props) => {
             longitude: -122.4324,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
-          }}
-        />
-          <View style={{position: 'absolute'}}>
+          }}>
 
-          <SearchBar />
+          {restaurants.map((r, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: r.lat,
+                longitude: r.lng,
+              }}
+            />
+          ))}
 
-          </View>
+        </MapView>
+        <View style={{position: 'absolute'}}>
+          <SearchBar onSearch={onCitySearch} />
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(_, index) => index.toString()}
+            data={cityList}
+            renderItem={({item}) => (
+              <City cityName={item} onSelect={() => onCitySelect(item)} />
+            )}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
